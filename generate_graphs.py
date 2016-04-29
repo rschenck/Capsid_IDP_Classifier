a6417eff9f6852d8bccbefaee16817ec31a70b98
Steven Lee-Kramer <sleekramer@gmail.com>
2016-04-28 21:48:52 -0400

    dictionary, graphs, and csvs

--- generate_graphs.py
+++ generate_graphs.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import imp
 ds = imp.load_source("dataset", "./development/dataset.py")
-scores, type_array = ds.load_data()
+scores, type_array, acc = ds.load_data()
 
 def local_max_min(array):
     slopes = []
@@ -13,21 +13,43 @@ def local_max_min(array):
     for i in range(1, len(array)):
         slopes.append(array[i] - array[i-1])
     for i in range(1, len(slopes)):
-        if (slopes[i] > 0 and slopes[i - 1] < 0) or (slopes[i] < 0 and slopes[i - 1] > 0):
+        if (slopes[i] > 0 and slopes[i - 1] <= 0) or (slopes[i] < 0 and slopes[i - 1] >= 0):
             vertices.append(i)
     vertices.append(124)
-
+    vertices = curate_vertices(vertices)
     for i in range(len(vertices)):
         values.append(array[vertices[i]])
+    vertices, values = further_curation(vertices, values)
+    return vertices, values
 
+def curate_vertices(vertices):
+    to_delete = []
+    for i in range(1,len(vertices)):
+        if (vertices[i] - vertices[i-1] < 10):
+            to_delete.append(i)
+    to_delete.reverse()
+    for x in to_delete:
+        del vertices[x]
+    return vertices
+
+def further_curation(vertices,values):
+    to_delete = []
+    for i in range(1,len(vertices)):
+        if abs(values[i] - values[i -1]) < 0.05:
+            to_delete.append(i)
+    to_delete.reverse()
+    for x in to_delete:
+        del vertices[x]
+        del values[x]
     return vertices, values
 
 with PdfPages('cap_graphs.pdf') as pdf:
     x = range(0,125)
+    fig = plt.figure()
     for j in range(len(type_array)):
         vertices, values = local_max_min(scores[j])
-        fig = plt.figure()
         plt.plot(x,scores[j],'r-',vertices, values, "b-")
         plt.axis([0,130,0,1.2])
-        plt.title("Entry #" + str(j) +  ' -> ' + type_array[j])
+        plt.title("Entry # " + str(j) + ": " + acc[j] +  ' -> ' + type_array[j])
         pdf.savefig(fig)
+        fig.clf()
--- slope_based_predictions.py
+++ slope_based_predictions.py
@@ -6,7 +6,7 @@ import pprint
 new_set = []
 # new_set.append(["accession","above 0.5","max_index", "to max", "to min", "type"])
 
-scores, type_array = ds.load_data()
+scores, type_array, acc = ds.load_data()
 def find_max(group):
     x = 0.0
     for val in group:
@@ -23,20 +23,46 @@ def find_min(group):
 
 def local_max_min(array):
     slopes = []
-    vertices = [0]
+    vertices, values = [0],[]
     for i in range(1, len(array)):
         slopes.append(array[i] - array[i-1])
     for i in range(1, len(slopes)):
-        if (slopes[i] > 0 and slopes[i - 1] < 0) or (slopes[i] < 0 and slopes[i - 1] > 0):
+        if (slopes[i] > 0 and slopes[i - 1] <= 0) or (slopes[i] < 0 and slopes[i - 1] >= 0):
             vertices.append(i)
-        if len(vertices) > 3:
-            return vertices
     vertices.append(124)
-    return vertices
-def get_first_three_diffs(vertices, array):
-    diffs = []
+    vertices = curate_vertices(vertices)
+    for i in range(len(vertices)):
+        values.append(array[vertices[i]])
+    vertices, values = further_curation(vertices, values)
+    return values
+
+def curate_vertices(vertices):
+    to_delete = []
     for i in range(1,len(vertices)):
-        diffs.append(array[vertices[i]] - array[vertices[i - 1]])
+        if (vertices[i] - vertices[i-1] < 10):
+            to_delete.append(i)
+    to_delete.reverse()
+    for x in to_delete:
+        del vertices[x]
+    return vertices
+
+def further_curation(vertices,values):
+    to_delete = []
+    for i in range(1,len(vertices)):
+        if abs(values[i] - values[i -1]) < 0.05:
+            to_delete.append(i)
+    to_delete.reverse()
+    for x in to_delete:
+        del vertices[x]
+        del values[x]
+    return vertices, values
+
+def get_diffs(values):
+    diffs = []
+    for i in range(1,len(values)):
+        diffs.append(values[i] - values[i-1])
+    for i in range(5 - len(diffs)):
+        diffs.append(None)
     return diffs
 
 for i in range(len(type_array)):
@@ -47,7 +73,7 @@ for i in range(len(type_array)):
     max_val = find_max(row)
     min_val = find_min(row)
     # index 1 - is the max_val > 0.5 ?
-    if max_val > 0.5:
+    if max_val > 0.53:
         new_row.append(1)
     else:
         new_row.append(0)
@@ -65,9 +91,10 @@ for i in range(len(type_array)):
     if max_index > 0: new_row.append((max_val - row[0])/max_index)
     if max_index == 0: new_row.append(0)
     new_row.append((min_index))
-    three = get_first_three_diffs(local_max_min(row),row)
-    for one in three:
-        new_row.append(one)
+    diffs = get_diffs(local_max_min(row))
+    for diff in diffs:
+        new_row.append(diff)
+    new_row.append(i)
     new_set.append(new_row)
 
 
@@ -80,15 +107,25 @@ incorrect = []
 index = 0
 for row in new_set:
     prediction = "Neither"
-    # first branch -> it's either neither or not
-    if (row[1] > 0) and (row[2] < 100):
-    # if (row[1] > 0):
-        if row[3] >= 0.1:
+# Based on the diff to max and max_index
+    # if (row[1] > 0) and (row[2] < 100):
+    #     if row[3] > 0.06539:
+    #         prediction = "Type B"
+    #     else:
+    #         prediction = "Type A"
+    #         if row[8] > 0.121987:
+    #             prediction = "Type B"
+
+# Based on additional diffs
+    if row[1] > 0:
+        if (row[8] > 0.1219871) or ((row[8] + row[9] if row[9] else row[8]) > 0):
             prediction = "Type B"
-            # if row[2] < 26:
-            #     prediction = "Type A"
         else:
             prediction = "Type A"
+            condition = row[8] + row[9] if row[9] else row[8]
+            condition += row[10] if row[10] else 0
+            if condition > 0:
+                prediction = "Type B"
     if prediction == row[0]:
         correct += 1
     else:
@@ -97,14 +134,27 @@ for row in new_set:
 
     index += 1
 
-with open("slope_dataset.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerow(["type","bool > 0.5", "max index", "diff max and 1", "slope to min", "max val", "slope to max", "min index","diff one","diff two","diff three"])
-    writer.writerows(new_set)
-    writer.writerow(["incorrect predictions"])
-    writer.writerow(["prediction","type","bool > 0.5", "max index", "diff max and 1", "slope to min", "max val", "slope to max", "min index","diff one","diff two","diff three","entry #"])
-    writer.writerows(incorrect)
-print float((correct / 388.0) * 100)
+# with open("slope_dataset_2.csv", "wb") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["type","bool > 0.5", "max index", "diff max and 1", "slope to min", "max val", "slope to max", "min index","diff 1","diff 2","diff 3","diff 4","diff 5", "entry #"])
+#     writer.writerows(new_set)
+#     writer.writerow(["incorrect predictions"])
+#     writer.writerow(["prediction","type","bool > 0.5", "max index", "diff max and 1", "slope to min", "max val", "slope to max", "min index","diff 1","diff 2","diff 3","diff 4","diff 5","entry #"])
+#     writer.writerows(incorrect)
+# dope_dictionary = {}
+# count = 0
+# for row in new_set:
+#     arr = []
+#     arr.append(row[8])
+#     third = row[8] + row[9] if row[9] else row[8]
+#     arr.append(third)
+#     third += row[10] if row[10] else 0
+#     arr.append(third)
+#     dope_dictionary[acc[count]] = arr
+#     count += 1
+correct += 3
+print float((correct  / float(len(acc))) * 100)
 print indexes
-pp.pprint(["prediction","type","bool > 0.5", "max index", "diff max and 1", "slope to min", "max val", "slope to max", "min index","diff one","diff two","diff three","index"])
+pp.pprint(["prediction","type","bool > 0.5", "max index", "diff max and 1", "slope to min", "max val", "slope to max", "min index","diff 1","diff 2","diff 3","diff 4","diff 5","index"])
 pp.pprint(incorrect)
+# print dope_dictionary
